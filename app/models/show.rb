24859862cb716719d83bdaafdff2c9678d8db52d
Stephen Harrington <stephenharrington04@yahoo.com>
2018-03-08 20:39:12 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,30 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
 
-  def most_popular_show
+  def self.most_popular_show
     Show.highest_rating.name
   end
 
-  def lowest_rating
+  def self.lowest_rating
     Show.minimum(:rating)
   end
 
-  def least_popular_show
+  def self.least_popular_show
     Show.lowest_rating.name
   end
 
-  def ratings_sum
+  def self.ratings_sum
     Show.sum(:rating)
   end
 
-  def popular_shows
+  def self.popular_shows
     Show.where(":rating > ?", 5)
   end
 
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     Show.order(:name)
   end
 
