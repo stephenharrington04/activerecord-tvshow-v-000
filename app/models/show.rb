0cb9607193bc95a01ad4bc2c514d5154dcc4640f
Stephen Harrington <stephenharrington04@yahoo.com>
2018-03-08 20:57:37 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.select("*").where(":rating = ?", self.highest_rating)
+    self.select("*").where("rating = ?", self.highest_rating)
   end
 
   def self.lowest_rating
